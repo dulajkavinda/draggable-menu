35bd792f3255afb57cc46f3728eb695b3a616292
Dulaj Kavinda <dulajkaavinda@gmail.com>
2021-02-14 10:53:39 +0530

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'minimal-switch'
+import 'minimal-switch/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
