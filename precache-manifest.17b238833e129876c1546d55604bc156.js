78b1138d8f252ad5e6f67fb11f828153232703da
Dulaj Kavinda <dulajkaavinda@gmail.com>
2021-02-18 10:11:58 +0530

    Updates

---
+++ precache-manifest.17b238833e129876c1546d55604bc156.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df6f08687ec46e6058fc8fe9f0c06d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06943aae9a51ba5e99d5",
+    "url": "./static/css/main.89666b08.chunk.css"
+  },
+  {
+    "revision": "d4cb7b247b353147f45a",
+    "url": "./static/js/2.d153ae81.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "./static/js/2.d153ae81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06943aae9a51ba5e99d5",
+    "url": "./static/js/main.e9c58fe9.chunk.js"
+  },
+  {
+    "revision": "88e4035f95615d001d8b",
+    "url": "./static/js/runtime-main.2b30836b.js"
+  }
+]);
